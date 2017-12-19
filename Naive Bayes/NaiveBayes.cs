a7a0570295a55b60d4398d93a5d697f93f58e3b1
RobertKraaijeveld <0890289@hr.nl>
2017-12-19 13:44:54 +0100

    x

--- Naive Bayes/NaiveBayes.cs
+++ Naive Bayes/NaiveBayes.cs
@@ -7,8 +7,8 @@ namespace NaiveBayesClassifier
     public class NaiveBayesClassifier
     {
         public List<Record> classifiedSet;
-        private Table<string, int> FrequencyTable = new Table<string, int>();
-        private Table<string, float> LikelihoodTable;
+        private Table<string, double> FrequencyTable = new Table<string, double>();
+        private Table<string, double> LikelihoodTable = new Table<string, double>();
 
         public NaiveBayesClassifier(List<Record> classifiedSet)
         {
@@ -22,31 +22,62 @@ namespace NaiveBayesClassifier
         {
             foreach (var record in classifiedSet)
             {
+                /*
                 //yay for containskey checks
                 if (!FrequencyTable.rowsPerColumn.ContainsKey(record.classification))
-                    FrequencyTable.rowsPerColumn.Add(record.classification, new Dictionary<string, int>());
+                    FrequencyTable.rowsPerColumn.Add(record.classification, new Dictionary<string, double>());
+                */
 
+                
                 foreach (var factor in record.Factors)
                 {
-                    if (!FrequencyTable.rowsPerColumn[record.classification].ContainsKey(factor.Key))
-                        FrequencyTable.rowsPerColumn[record.classification].Add(factor.Key, 0);
+                    if (!FrequencyTable[record.classification].ContainsKey(factor.Key))
+                        FrequencyTable[record.classification].Add(factor.Key, 0);
 
                     if (factor.Value == true)
                     {
-                        FrequencyTable.rowsPerColumn[record.classification][factor.Key] += 1;
+                        FrequencyTable[record.classification][factor.Key] += 1;
                     }
                 }
             }
 
-            foreach (var row in FrequencyTable.rowsPerColumn["Conservative"])
-            {
+            foreach (var row in FrequencyTable["Conservative"])
                 Console.WriteLine("Factor " + row.Key + " shows up " + row.Value + " times for conservatives");
-            }
         }
 
         private void CreateLikelihoodTable()
         {
+            LikelihoodTable = FrequencyTable;
+            foreach (var record in FrequencyTable.rowsPerColumn)
+            {
+                var classificationOfItem = record.Item1;
+                Console.WriteLine("LIKELIHOOD: classificationOfItem = " + classificationOfItem);
+                //The corresponding item in the likelihoodtable is the value / the amount with that same class
 
+                foreach (var factor in record.Item2)
+                {
+                    LikelihoodTable[classificationOfItem][factor.Key] = factor.Value / 1;//GetCountsOfClassifications(LikelihoodTable);
+                }
+            }
+        }
+
+        private Dictionary<string, int> GetCountsOfClassifications(Table<string, double> table, string classification)
+        {
+            Dictionary<string, int> CountsForGivenClassification = new Dictionary<string, int>();
+
+            //rowsPerColumn dictionary needs to be a list (?)
+            //Nah, just make sure the AMOUNT of classifications of a certain kind is known somewhere
+            foreach (var record in table.rowsPerColumn)
+            {
+                if (record.Item1 == classification)
+                {
+                    if (!CountsForGivenClassification.ContainsKey(record.Item1))
+                        CountsForGivenClassification.Add(record.Item1, 1);
+                    else
+                        CountsForGivenClassification[record.Item1] += 1;
+                }
+            }
+            return CountsForGivenClassification;
         }
     }
 }
--- Naive Bayes/Table.cs
+++ Naive Bayes/Table.cs
@@ -7,11 +7,19 @@ namespace NaiveBayesClassifier
     public class Table<T, J>
     {
         //Key is classification name, tablenetry key is factors name, tableentry value is factors count for this classification. 
-        public Dictionary<T, Dictionary<T, J>> rowsPerColumn { get; set; }
+        //This is a list because we need to be able to store duplicate classificationss
+        public List<Tuple<T, Dictionary<T, J>>> rowsPerColumn { get; set; }
+
+
+        //Overloading square bracket operator 
+        public Dictionary<T, J> this[string i]
+        {
+            get { return rowsPerColumn.Where(r => r.Item1.Equals(i)).First().Item2; }
+        }
 
         public Table()
         {
-            rowsPerColumn = new Dictionary<T, Dictionary<T, J>>();
+            rowsPerColumn = new List<Tuple<T, Dictionary<T, J>>>();
         }
     }
 }
